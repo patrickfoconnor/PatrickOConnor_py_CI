efc6f4d0e5885667e68693d7d2051d1e19661c23
Patrick O'Connor <oconnor.p2016@gmail.com>
2022-02-14 11:44:05 -0700

    Init .gitignore
    Init calculations.py
    - Two test one should pass from start and one will fail

---
+++ calculations.py
@@ -0,0 +1,22 @@
+# Testing simple addition and subtraction calculations
+
+# Simple addition function that should pass
+def addition(a, b):
+    product = a - b
+    return product
+
+# Simple subtraction function that should pass
+def subtraction(a, b):
+    difference = a - b
+    return difference
+
+# Test addition function
+# Should return failure
+def test_addition():
+    assert addition(1, 1) == 2
+
+# Test subtraction function
+# Should pass with flying colors
+def test_subtraction():
+    assert subtraction(1, 1) == 0
+
